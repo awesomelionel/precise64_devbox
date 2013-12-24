fdc522bfaeb625c7c0d82d233e00f465ea79e090
Lionel MBA13 <lionel.tan@zalora.com.my>
2013-12-24 15:16:41 +0800

    Setup Vagrant Box, provisioned with MySQL, Rbenv, Ruby 2.0, Php 5.3.5, apache2 and vim

---
+++ manifests/site.pp
@@ -0,0 +1,19 @@
+Exec { 	path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+}
+exec {"apt-update": command => "/usr/bin/apt-get update"}
+Exec["apt-update"] -> Package <| |> 
+
+include puppi
+include apache2
+include mysql
+include vim
+include php
+include curl
+
+class {'curl':}
+
+class { 'rbenv': }
+rbenv::plugin { [ 'sstephenson/rbenv-vars', 'sstephenson/ruby-build' ]: }
+rbenv::build { '2.0.0-p247': global => true }
+rbenv::gem { 'thor': ruby_version   => '2.0.0-p247' }
+
---
+++ modules/apache2/manifests/init.pp
@@ -0,0 +1,30 @@
+class apache2{
+
+	package {
+		"apache2":
+			ensure => present,
+			before => File["/etc/apache2/apache2.conf"]
+	}
+
+	service {
+		"apache2":
+			ensure => true,
+			enable => true,
+			subscribe => File["/etc/apache2/apache2.conf"]
+	}
+
+	file {
+		"/etc/apache2/apache2.conf":
+			 owner  => root,
+			 group  => root,
+			 mode   => 644,
+			source => "puppet:///modules/apache2/apache2.conf";
+			/*"/etc/apache2/sites-enabled/testapp.conf":*/
+			/*source => "puppet:///modules/apache2/testapp.conf",*/
+			 /*owner  => root,*/
+			 /*group  => root,*/
+			 /*notify => Service["apache2"],*/
+			 /*require => Package["apache2"];*/
+	}
+
+}
